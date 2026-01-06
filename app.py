d53cf3aa3fb1c1db34105e6bb154018c3fa21b76
Nithin969 <165005123+Nithin969@users.noreply.github.com>
2026-01-06 14:19:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+# Home page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# About page
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+# Project page
+@app.route("/project")
+def project():
+    return render_template("project.html")
+
+# Contact page
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+# Login page
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+# Thank you page
+@app.route("/thankyou")
+def thankyou():
+    return render_template("thankyou.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
